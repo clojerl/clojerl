cbaa804f4a4b63f14ced6fc26464a2720c53a5cf
Juan Facorro <juan.facorro@gmail.com>
2015-07-22 01:15:00 -0300

    Read integers, floats and ratios

--- src/clj_reader.erl
+++ src/clj_reader.erl
@@ -1,74 +1,106 @@
 -module(clj_reader).
 
--export([read/1]).
+-export([read/1, read_all/1]).
 
 -type attrs() :: #{}.
 -type ast_node() :: #{type => atom,
                       attrs => attrs(),
                       children => [atom()]}.
 
+-type state() :: #{src => binary(),
+                   forms => [ast_node()]}.
+
 -spec read(binary()) -> ast_node().
 read(Src) ->
-  dispatch(Src).
+  State = #{src => Src, forms => []},
+  #{forms := Forms} = dispatch(State),
+  hd(Forms).
 
-dispatch(<<>>) ->
-  eof;
-dispatch(<<First, Rest/binary>> = Src) ->
-  case type_start(First, Rest) of
-    whitespace -> dispatch(Rest);
-    number -> read_number(Src);
-    string -> read_string(Src);
-    keyword -> read_keyword(Src);
-    comment -> read_comment(Src);
-    quote -> read_quote(Src);
-    deref -> read_deref(Src);
-    meta -> read_meta(Src);
-    syntax_quote -> read_syntax_quote(Src);
-    unquote -> read_unquote(Src);
-    list -> read_list(Src);
-    vector -> read_vector(Src);
-    map -> read_map(Src);
-    unmatched_delim -> read_unmatched_delim(Src);
-    char -> read_char(Src);
-    arg -> read_arg(Src);
-    dispatch -> read_dispatch(Src);
-    symbol -> read_symbol(Src)
-  end.
+-spec read_all(state()) -> [ast_node()].
+read_all(Src) ->
+  State = #{src => Src,
+            forms => [],
+            all => true},
+  #{forms := Forms} = dispatch(State),
+  lists:reverse(Forms).
 
-type_start(X, _)
+-spec dispatch(state()) -> state().
+dispatch(#{src := <<>>} = State) ->
+  State;
+dispatch(#{src := Src} = State) ->
+  <<First, Rest/binary>> = Src,
+  NewState = case type_char(First, Rest) of
+               whitespace -> dispatch(State#{src => Rest});
+               number -> read_number(State);
+               string -> read_string(State);
+               keyword -> read_keyword(State);
+               comment -> read_comment(State);
+               quote -> read_quote(State);
+               deref -> read_deref(State);
+               meta -> read_meta(State);
+               syntax_quote -> read_syntax_quote(State);
+               unquote -> read_unquote(State);
+               list -> read_list(State);
+               vector -> read_vector(State);
+               map -> read_map(State);
+               unmatched_delim -> read_unmatched_delim(State);
+               char -> read_char(State);
+               arg -> read_arg(State);
+               dispatch -> read_dispatch(State);
+               symbol -> read_symbol(State)
+             end,
+  next(NewState).
+
+-spec next(state()) -> state().
+next(#{all := true} = State) -> dispatch(State);
+next(State) -> State.
+
+-spec type_char(non_neg_integer(), binary()) -> atom().
+type_char(X, _)
   when X == $\n; X == $\t; X == $\r; X == $ ; X == $,->
   whitespace;
-type_start(X, _)
+type_char(X, _)
   when X >= $0, X =< $9 ->
   number;
-type_start(X, <<Y, _/binary>>)
+type_char(X, <<Y, _/binary>>)
   when (X == $+ orelse X == $-),
        Y >= $0, Y =< $9 ->
   number;
-type_start($", _) -> string;
-type_start($:, _) -> keyword;
-type_start($;, _) -> comment;
-type_start($', _) -> quote;
-type_start($@, _) -> deref;
-type_start($^, _) -> meta;
-type_start($`, _) -> syntax_quote;
-type_start($~, _) -> unquote;
-type_start($(, _) -> list;
-type_start($[, _) -> vector;
-type_start(${, _) -> map;
-type_start(X, _) when X == $); X == $]; X == $} ->
+type_char($", _) -> string;
+type_char($:, _) -> keyword;
+type_char($;, _) -> comment;
+type_char($', _) -> quote;
+type_char($@, _) -> deref;
+type_char($^, _) -> meta;
+type_char($`, _) -> syntax_quote;
+type_char($~, _) -> unquote;
+type_char($(, _) -> list;
+type_char($[, _) -> vector;
+type_char(${, _) -> map;
+type_char(X, _)
+  when X == $); X == $]; X == $} ->
   unmatched_delim;
-type_start($\\, _) -> char;
-type_start($%, _) -> arg;
-type_start($#, _) -> dispatch;
-type_start(_, _) -> symbol.
+type_char($\\, _) -> char;
+type_char($%, _) -> arg;
+type_char($#, _) -> dispatch;
+type_char(_, _) -> symbol.
 
-read_number(_) -> number.
+-spec read_number(state()) -> state().
+read_number(#{forms := Forms, src := Src} = State) ->
+  {SrcRest, Current} = consume(Src, [number, symbol]),
+  Number = clj_utils:parse_number(Current),
+  State#{forms => [Number | Forms],
+         src => SrcRest}.
+
 read_string(_) -> string.
 read_keyword(_) -> keyword.
 read_comment(_) -> comment.
 read_quote(_) -> quote.
-read_deref(_) -> deref.
+read_deref(#{forms := Forms,
+             src := <<_, Src/binary>>} = State) ->
+  State#{forms => [deref | Forms],
+         src => Src}.
+
 read_meta(_) -> meta.
 read_syntax_quote(_) -> syntax_quote.
 read_unquote(_) -> unquote.
@@ -80,3 +112,15 @@ read_char(_) -> char.
 read_arg(_) -> arg.
 read_dispatch(_) -> dispatch.
 read_symbol(_) -> symbol.
+
+consume(Src, Types) ->
+  do_consume(Src, <<>>, Types).
+
+do_consume(<<>>, Acc, _Types) ->
+  {<<>>, Acc};
+do_consume(<<X, Rest/binary>> = Src, Acc, Types) ->
+  Type = type_char(X, Rest),
+  case lists:member(Type, Types) of
+    true -> do_consume(Rest, <<Acc/binary, X>>, Types);
+    false -> {Src, Acc}
+  end.
---
+++ src/clj_utils.erl
@@ -0,0 +1,105 @@
+-module(clj_utils).
+
+-export([
+         parse_number/1
+        ]).
+
+-define(INT_PATTERN,
+        "^([-+]?)"
+        "(?:(0)|([1-9][0-9]*)|0[xX]([0-9A-Fa-f]+)|0([0-7]+)|"
+        "([1-9][0-9]?)[rR]([0-9A-Za-z]+)|0[0-9]+)(N)?$").
+-define(FLOAT_PATTERN, "^(([-+]?[0-9]+)(\.[0-9]*)?([eE][-+]?[0-9]+)?)(M)?$").
+-define(RATIO_PATTERN, "^([-+]?[0-9]+)/([0-9]+)$").
+
+parse_number(Number) ->
+  Result = case number_type(Number) of
+             int       -> parse_int(Number);
+             float     -> parse_float(Number);
+             ratio     -> parse_ratio(Number);
+             undefined -> undefined
+           end,
+
+  case Result of
+    undefined ->
+      throw(<<"Invalid number format [", Number/binary, "]">>);
+    _ ->
+      Result
+  end.
+
+%% @doc Valid integers can be either in decimal, octal, hexadecimal or any
+%%      base specified (e.g. `2R010` is binary for `2`).
+parse_int(IntBin) ->
+  {match, [_ | Groups]} = re:run(IntBin, ?INT_PATTERN, [{capture, all, list}]),
+  case int_properties(Groups) of
+    {zero, _Arbitrary, _Negate} ->
+      0;
+    {{Base, Value}, _Arbitrary, Negate} ->
+      list_to_integer(Value, Base) * Negate;
+    _ ->
+      undefined
+  end.
+
+int_properties(Groups) ->
+  Props = lists:map(fun(X) -> X =/= "" end, Groups),
+  Result =
+    case Props of
+      [_, true | _] -> zero;
+      [_, _, true | _]-> {10, nth(3, Groups)};
+      [_, _, _, true | _]-> {16, nth(4, Groups)};
+      [_, _, _, _, true | _]-> {8, nth(5, Groups)};
+      [_, _, _, _, _ ,_, true | _]->
+        Base = list_to_integer(lists:nth(6, Groups)),
+        {Base, lists:nth(7, Groups)}
+    end,
+
+  Arbitrary = nth(8, Props, false),
+  Negate = nth(1, Props),
+  {Result, Arbitrary, case Negate of true -> -1; false -> 1 end}.
+
+parse_float(FloatBin) ->
+  {match, [_ | Groups]} = re:run(FloatBin, ?FLOAT_PATTERN, [{capture, all, list}]),
+  Decimal = nth(3, Groups, "") =/= "",
+  case Decimal of
+    true ->
+      FloatStr = nth(1, Groups),
+      list_to_float(FloatStr);
+    false ->
+      %% When there is no decimal part we add it so we can use
+      %% list_to_float/1.
+      FloatStr = nth(2, Groups) ++ ".0" ++ nth(4, Groups),
+      list_to_float(FloatStr)
+  end.
+
+parse_ratio(RatioBin) ->
+  {match, [_ | Groups]} = re:run(RatioBin, ?RATIO_PATTERN, [{capture, all, list}]),
+  Numerator = nth(1, Groups),
+  Denominator = nth(2, Groups),
+  {ratio,
+   list_to_integer(Numerator),
+   list_to_integer(Denominator)}.
+
+number_type(Number) ->
+  Regex = #{int => ?INT_PATTERN,
+            float => ?FLOAT_PATTERN,
+            ratio => ?RATIO_PATTERN},
+  Fun = fun(Type, RE, Acc) ->
+            case re:run(Number, RE) of
+              nomatch -> Acc;
+              _ -> [Type | Acc]
+            end
+        end,
+  case maps:fold(Fun, [], Regex) of
+    [] -> undefined;
+    [T | _] -> T
+  end.
+
+%% @doc Like lists:nth/2 but returns `undefined` if `Index` is
+%%      larger than the amount of elements in `List`.
+nth(Index, List) ->
+  nth(Index, List, undefined).
+
+nth(Index, List, Default) ->
+  case Index =< length(List) of
+    true -> lists:nth(Index, List);
+    false -> Default
+  end.
