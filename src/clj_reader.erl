e85c88e535017248bc9ad4c50e0a512bcaf1420a
Juan Facorro <juan.facorro@gmail.com>
2015-07-23 00:26:12 -0300

    Read strings, escaped unicode chars missing

--- src/clj_reader.erl
+++ src/clj_reader.erl
@@ -10,6 +10,14 @@
 -type state() :: #{src => binary(),
                    forms => [ast_node()]}.
 
+-type char_type() :: whitespace | number | string
+                   | keyword | comment | quote
+                   | deref | meta | syntax_quote
+                   | unquote | list | vector
+                   | map | unmatched_delim | char
+                   | unmatched_delim | char
+                   | arg | dispatch | symbol.
+
 -spec read(binary()) -> ast_node().
 read(Src) ->
   State = #{src => Src, forms => []},
@@ -29,7 +37,7 @@ dispatch(#{src := <<>>} = State) ->
   State;
 dispatch(#{src := Src} = State) ->
   <<First, Rest/binary>> = Src,
-  NewState = case type_char(First, Rest) of
+  NewState = case char_type(First, Rest) of
                whitespace -> dispatch(State#{src => Rest});
                number -> read_number(State);
                string -> read_string(State);
@@ -55,71 +63,123 @@ dispatch(#{src := Src} = State) ->
 next(#{all := true} = State) -> dispatch(State);
 next(State) -> State.
 
--spec type_char(non_neg_integer(), binary()) -> atom().
-type_char(X, _)
+-spec char_type(non_neg_integer(), binary()) -> char_type().
+char_type(X, _)
   when X == $\n; X == $\t; X == $\r; X == $ ; X == $,->
   whitespace;
-type_char(X, _)
+char_type(X, _)
   when X >= $0, X =< $9 ->
   number;
-type_char(X, <<Y, _/binary>>)
+char_type(X, <<Y, _/binary>>)
   when (X == $+ orelse X == $-),
        Y >= $0, Y =< $9 ->
   number;
-type_char($", _) -> string;
-type_char($:, _) -> keyword;
-type_char($;, _) -> comment;
-type_char($', _) -> quote;
-type_char($@, _) -> deref;
-type_char($^, _) -> meta;
-type_char($`, _) -> syntax_quote;
-type_char($~, _) -> unquote;
-type_char($(, _) -> list;
-type_char($[, _) -> vector;
-type_char(${, _) -> map;
-type_char(X, _)
+char_type($", _) -> string;
+char_type($:, _) -> keyword;
+char_type($;, _) -> comment;
+char_type($', _) -> quote;
+char_type($@, _) -> deref;
+char_type($^, _) -> meta;
+char_type($`, _) -> syntax_quote;
+char_type($~, _) -> unquote;
+char_type($(, _) -> list;
+char_type($[, _) -> vector;
+char_type(${, _) -> map;
+char_type(X, _)
   when X == $); X == $]; X == $} ->
   unmatched_delim;
-type_char($\\, _) -> char;
-type_char($%, _) -> arg;
-type_char($#, _) -> dispatch;
-type_char(_, _) -> symbol.
+char_type($\\, _) -> char;
+char_type($%, _) -> arg;
+char_type($#, _) -> dispatch;
+char_type(_, _) -> symbol.
 
 -spec read_number(state()) -> state().
 read_number(#{forms := Forms, src := Src} = State) ->
   {SrcRest, Current} = consume(Src, [number, symbol]),
   Number = clj_utils:parse_number(Current),
   State#{forms => [Number | Forms],
-         src => SrcRest}.
+         src   => SrcRest}.
 
-read_string(_) -> string.
+-spec read_string(state()) -> state().
+read_string(#{forms := Forms,
+              src := <<"\"", SrcRest/binary>>,
+              current := String} = State0) ->
+  State = maps:remove(current, State0),
+  State#{forms => [String | Forms],
+         src => SrcRest};
+read_string(#{src := <<"\\", SrcRest/binary>>,
+              current := String} = State0) ->
+  {EscapedChar, Rest} = escape_char(SrcRest),
+  State = State0#{current => <<String/binary,
+                               EscapedChar/binary>>,
+                  src => Rest},
+  read_string(State);
+read_string(#{src := <<Char, SrcRest/binary>>,
+              current := String} = State0) ->
+  State = State0#{current => <<String/binary, Char>>,
+                  src => SrcRest},
+  read_string(State);
+read_string(#{src := <<"\"", Rest/binary>>} = State) ->
+  read_string(State#{src => Rest, current => <<>>});
+read_string(#{src := <<>>}) ->
+  throw(<<"EOF while reading string">>).
+
+-spec escape_char(binary()) -> {binary(), binary()}.
+escape_char(<<Char, Rest/binary>>) ->
+  Escaped =
+    case Char of
+      $t -> <<"\t">>;
+      $r -> <<"\r">>;
+      $n -> <<"\n">>;
+      $\ -> <<"\\">>;
+      $" -> <<"\"">>;
+      $b -> <<"\b">>;
+      $f -> <<"\f">>;
+      $u -> <<"unicode">>
+    end,
+  {Escaped, Rest}.
+
 read_keyword(_) -> keyword.
+
 read_comment(_) -> comment.
+
 read_quote(_) -> quote.
+
 read_deref(#{forms := Forms,
              src := <<_, Src/binary>>} = State) ->
   State#{forms => [deref | Forms],
          src => Src}.
 
 read_meta(_) -> meta.
+
 read_syntax_quote(_) -> syntax_quote.
+
 read_unquote(_) -> unquote.
+
 read_list(_) -> list.
+
 read_vector(_) -> vector.
+
 read_map(_) -> map.
+
 read_unmatched_delim(_) -> throw(unmatched_delim).
+
 read_char(_) -> char.
+
 read_arg(_) -> arg.
+
 read_dispatch(_) -> dispatch.
+
 read_symbol(_) -> symbol.
 
+-spec consume(binary(), [char_type()]) -> {binary(), binary()}.
 consume(Src, Types) ->
   do_consume(Src, <<>>, Types).
 
 do_consume(<<>>, Acc, _Types) ->
   {<<>>, Acc};
 do_consume(<<X, Rest/binary>> = Src, Acc, Types) ->
-  Type = type_char(X, Rest),
+  Type = char_type(X, Rest),
   case lists:member(Type, Types) of
     true -> do_consume(Rest, <<Acc/binary, X>>, Types);
     false -> {Src, Acc}
--- src/clj_utils.erl
+++ src/clj_utils.erl
@@ -11,6 +11,9 @@
 -define(FLOAT_PATTERN, "^(([-+]?[0-9]+)(\.[0-9]*)?([eE][-+]?[0-9]+)?)(M)?$").
 -define(RATIO_PATTERN, "^([-+]?[0-9]+)/([0-9]+)$").
 
+-type ratio() :: {ratio, integer(), integer()}.
+
+-spec parse_number(binary()) -> integer() | float() | ratio().
 parse_number(Number) ->
   Result = case number_type(Number) of
              int       -> parse_int(Number);
@@ -28,6 +31,7 @@ parse_number(Number) ->
 
 %% @doc Valid integers can be either in decimal, octal, hexadecimal or any
 %%      base specified (e.g. `2R010` is binary for `2`).
+-spec parse_int(binary()) -> integer() | undefined.
 parse_int(IntBin) ->
   {match, [_ | Groups]} = re:run(IntBin, ?INT_PATTERN, [{capture, all, list}]),
   case int_properties(Groups) of
@@ -39,6 +43,9 @@ parse_int(IntBin) ->
       undefined
   end.
 
+-spec int_properties([string()]) -> {zero | undefined | {integer(), string()},
+                                     boolean(),
+                                     boolean()}.
 int_properties(Groups) ->
   Props = lists:map(fun(X) -> X =/= "" end, Groups),
   Result =
@@ -49,13 +56,16 @@ int_properties(Groups) ->
       [_, _, _, _, true | _]-> {8, nth(5, Groups)};
       [_, _, _, _, _ ,_, true | _]->
         Base = list_to_integer(lists:nth(6, Groups)),
-        {Base, lists:nth(7, Groups)}
+        {Base, lists:nth(7, Groups)};
+      _ ->
+        undefined
     end,
 
   Arbitrary = nth(8, Props, false),
   Negate = nth(1, Props),
   {Result, Arbitrary, case Negate of true -> -1; false -> 1 end}.
 
+-spec parse_float(binary()) -> float().
 parse_float(FloatBin) ->
   {match, [_ | Groups]} = re:run(FloatBin, ?FLOAT_PATTERN, [{capture, all, list}]),
   Decimal = nth(3, Groups, "") =/= "",
@@ -70,6 +80,7 @@ parse_float(FloatBin) ->
       list_to_float(FloatStr)
   end.
 
+-spec parse_ratio(binary()) -> ratio().
 parse_ratio(RatioBin) ->
   {match, [_ | Groups]} = re:run(RatioBin, ?RATIO_PATTERN, [{capture, all, list}]),
   Numerator = nth(1, Groups),
@@ -79,7 +90,7 @@ parse_ratio(RatioBin) ->
    list_to_integer(Denominator)}.
 
 number_type(Number) ->
-  Regex = #{int => ?INT_PATTERN,
+  Regex = #{int   => ?INT_PATTERN,
             float => ?FLOAT_PATTERN,
             ratio => ?RATIO_PATTERN},
   Fun = fun(Type, RE, Acc) ->
