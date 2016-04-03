f1efb273d6405d460d623687b8161415d1e4f2e1
Juan Facorro <juan.facorro@gmail.com>
2016-04-03 13:49:51 +0200

    [#89] Implemented naive caching for protocol resolution

--- src/lang/clojerl.protocol.erl
+++ src/lang/clojerl.protocol.erl
@@ -7,45 +7,64 @@
 
 -spec resolve(atom(), atom(), list()) -> any().
 resolve(Protocol, FunctionName, Args = [Head | _]) ->
-  TypeModule   = clj_core:type(Head),
-  ImplFunction = impl_function(Protocol, FunctionName),
+  Type = clj_core:type(Head),
 
-  IsExported = erlang:function_exported(TypeModule, ImplFunction, length(Args)),
+  case resolve_impl_cache(Protocol, FunctionName, Type, length(Args)) of
+    {Module, Function} ->
+      apply(Module, Function, Args);
+    undefined ->
+      TypeBin = atom_to_binary(Type, utf8),
+      FunctionBin = atom_to_binary(FunctionName, utf8),
+      ProtocolBin = atom_to_binary(Protocol, utf8),
+      Value = clj_core:str(Head),
+      error(<<"Type '", TypeBin/binary, "'"
+              " has no implementation for function '",
+              FunctionBin/binary,
+              "' in protocol '",
+              ProtocolBin/binary, "' ",
+              "(value = ", Value/binary, ", args = ",
+              (clj_core:str(Args))/binary,
+              ")">>)
+  end.
 
-  case IsExported of
-    true ->
-      apply(TypeModule, ImplFunction, Args);
+-spec resolve_impl_cache(atom(), atom(), atom(), integer()) ->
+  {module(), atom()} | undefined.
+resolve_impl_cache(Protocol, Function, Type, Arity) ->
+  Key = {resolve_impl, Protocol, Function, Type, Arity},
+  case erlang:get(Key) of
+    undefined ->
+      Value = resolve_impl(Protocol, Function, Type, Arity),
+      erlang:put(Key, Value),
+      Value;
+     Value -> Value
+  end.
+
+-spec resolve_impl(atom(), atom(), atom(), integer()) -> {module(), atom()} | undefined.
+resolve_impl(Protocol, Function, Type, Arity) ->
+  ImplFunction = impl_function(Protocol, Function),
+  case erlang:function_exported(Type, ImplFunction, Arity) of
+    true  -> {Type, ImplFunction};
     false ->
-      ImplModule = impl_module(Protocol, TypeModule),
-      IsExported2 =
-        erlang:function_exported(ImplModule, FunctionName, length(Args)),
-      case IsExported2 of
-        true ->
-          apply(TypeModule, ImplFunction, Args);
-        false ->
-          TypeBin = atom_to_binary(TypeModule, utf8),
-          FunctionBin = atom_to_binary(FunctionName, utf8),
-          ProtocolBin = atom_to_binary(Protocol, utf8),
-          Value = clj_core:str(Head),
-          error(<<"Type '", TypeBin/binary, "'"
-                  " has no implementation for function '",
-                  FunctionBin/binary,
-                  "' in protocol '",
-                  ProtocolBin/binary, "' ",
-                  "(value = ", Value/binary, ", args = ",
-                  (clj_core:str(Args))/binary,
-                  ")">>)
-        end
+      ImplModule = impl_module(Protocol, Type),
+      case erlang:function_exported(ImplModule, Function, Arity) of
+        true  -> {ImplModule, Function};
+        false -> undefined
+      end
   end.
 
 -spec 'extends?'(atom(), atom()) -> boolean().
 'extends?'(Protocol, Type) ->
-  (erlang:function_exported(Type, module_info, 1)
-   andalso
-   lists:keymember([Protocol], 2, Type:module_info(attributes))
-  )
-    orelse
-    code:is_loaded(impl_module(Protocol, Type)) =/= false.
+  Key = {extends, Protocol, Type},
+  case erlang:get(Key) of
+    undefined ->
+      Value = (erlang:function_exported(Type, module_info, 1)
+               andalso
+               lists:keymember([Protocol], 2, Type:module_info(attributes))
+              ) orelse code:is_loaded(impl_module(Protocol, Type)) =/= false,
+      erlang:put(Key, Value),
+      Value;
+    Value -> Value
+  end.
 
 -spec impl_module(atom(), atom()) -> atom().
 impl_module(Protocol, Type) when is_atom(Protocol),
