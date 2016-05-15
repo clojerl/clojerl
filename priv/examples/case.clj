a068d40ed5728ae2c47a032062b747914c512762
Juan Facorro <juan.facorro@gmail.com>
2016-05-15 17:59:26 +0200

    [#73] Introduce tuple to clojure file

--- priv/examples/case.clj
+++ priv/examples/case.clj
@@ -7,7 +7,7 @@
                 :c 3)
        z (case* y
                 x 2
-                :default)]
+                #[:default :value])]
   (erlang/display.e x)
   (erlang/display.e y)
   (erlang/display.e z))
