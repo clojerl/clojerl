7bb7eefa720c9235236f45f79c886b69502f6506
Juan Facorro <juan.facorro@gmail.com>
2016-05-23 14:03:13 +0200

    [#140] Add update function in scope that only modifies existing key

--- src/clj_scope.erl
+++ src/clj_scope.erl
@@ -3,8 +3,9 @@
 -export([ new/0
         , new/1
         , parent/1
-        , put/3
         , get/2
+        , put/3
+        , update/3
         , to_map/2
         , from_map/1
         ]).
@@ -27,13 +28,17 @@ new(Parent) ->
 parent(undefined) -> undefined;
 parent(Scope)     -> maps:get(parent, Scope).
 
+-spec get(scope(), any()) -> any().
+get(Scope, Key) ->
+  do_get(Scope, Key).
+
 -spec put(scope(), any(), any()) -> scope().
 put(Scope = #{mappings := Mappings}, Key, Value) ->
   Scope#{mappings => Mappings#{Key => Value}}.
 
--spec get(scope(), any()) -> any().
-get(Scope, Key) ->
-  do_get(Scope, Key).
+-spec update(scope(), any(), any()) -> scope().
+update(Scope, Key, Value) ->
+  do_update(Scope, Key, Value).
 
 -spec to_map(scope(), function()) -> any().
 to_map(Scope, Fun) ->
@@ -64,4 +69,16 @@ do_get(Scope = #{mappings := Mappings}, Key) ->
       do_get(parent(Scope), Key);
     true ->
       maps:get(Key, Mappings)
+  end.
+
+%% @private
+-spec do_update(scope() | undefined, any(), any()) -> scope().
+do_update(undefined, K, _) ->
+  throw({not_found, K});
+do_update(Scope = #{mappings := Mappings, parent := Parent}, K, V) ->
+  case maps:is_key(K, Mappings) of
+    true ->
+      Scope#{mappings => Mappings#{K => V}};
+    false ->
+      Scope#{parent => do_update(Parent, K, V)}
   end.
