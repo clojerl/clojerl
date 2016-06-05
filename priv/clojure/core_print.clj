9e4771c61b0b914f39afe4dc0404b85b3e14c0cf
Juan Facorro <juan.facorro@klarna.com>
2016-06-05 23:05:06 +0200

    [#148] Add methods for :clojerl.LazySeq

--- priv/clojure/core_print.clj
+++ priv/clojure/core_print.clj
@@ -128,9 +128,14 @@
   (print-meta o w)
   (print-sequential "(" pr-on " " ")" o w))
 
+(defmethod print-method :clojerl.LazySeq [o, ^Writer w]
+  (print-meta o w)
+  (print-sequential "(" pr-on " " ")" o w))
+
 (defmethod print-dup :clojerl.List [o w] (print-method o w))
 (defmethod print-dup :clojerl.erlang.List [o w] (print-method o w))
 (defmethod print-dup :clojerl.Cons [o w] (print-method o w))
+(defmethod print-dup :clojerl.LazySeq [o w] (print-method o w))
 
 (def ^{:tag String
        :doc "Returns escape string for char or nil if none"
