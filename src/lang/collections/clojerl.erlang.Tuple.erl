10dc9b89ff853195001fe1828f9ba12d4340b9b9
Juan Facorro <juan.facorro@gmail.com>
2016-08-12 17:49:29 +0200

    [#102] Add clojerl.Indexed implementation to Tuple

--- src/lang/collections/clojerl.erlang.Tuple.erl
+++ src/lang/collections/clojerl.erlang.Tuple.erl
@@ -3,12 +3,16 @@
 -behavior('clojerl.Counted').
 -behavior('clojerl.IHash').
 -behavior('clojerl.ISequential').
+-behavior('clojerl.Indexed').
 -behavior('clojerl.Seqable').
 -behavior('clojerl.Stringable').
 
 -export([count/1]).
 -export([hash/1]).
 -export(['_'/1]).
+-export([ nth/2
+        , nth/3
+        ]).
 -export([seq/1]).
 -export([str/1]).
 
@@ -21,6 +25,15 @@ count(Tuple) -> tuple_size(Tuple).
 hash(Tuple) -> clj_murmur3:ordered(Tuple).
 
 '_'(_) -> undefined.
+
+nth(Tuple, N) ->
+  erlang:element(N + 1, Tuple).
+
+nth(Tuple, N, NotFound) ->
+  case N >= erlang:size(Tuple) of
+    true  -> NotFound;
+    false -> erlang:element(N + 1, Tuple)
+  end.
 
 seq({}) -> undefined;
 seq(Tuple) -> tuple_to_list(Tuple).
