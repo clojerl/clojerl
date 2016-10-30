cfcaa8e719a6b637b46619cf70bb3887103d3c86
Juan Facorro <juan.facorro@gmail.com>
2016-10-30 22:31:34 +0100

    [#122] Create different cerl_eval modules to avoid compiling processes still evaluating being killed

--- src/clj_compiler.erl
+++ src/clj_compiler.erl
@@ -2,25 +2,22 @@
 
 -include("clojerl.hrl").
 
--export([
-         compile_files/1,
-         compile_files/2,
-         compile_file/1,
-         compile_file/2,
-         compile/1,
-         compile/2,
-         load_file/1,
-         eval/1,
-         eval/2,
-         eval/3,
-         compile_forms/2
+-export([ compile_files/1
+        , compile_files/2
+        , compile_file/1
+        , compile_file/2
+        , compile/1
+        , compile/2
+        , load_file/1
+        , eval/1
+        , eval/2
+        , eval/3
+        , compile_forms/2
         ]).
 
 -export([ no_warn_dynamic_var_name/1
         , no_warn_symbol_as_erl_fun/1
         ]).
-
--export([ast_to_string/1]).
 
 -type clj_flag() :: 'no-warn-symbol-as-erl-fun'
                   | 'no-warn-dynamic-var-name'.
@@ -291,10 +288,7 @@ compile_forms_fun(Opts) ->
       fun(Forms) -> compile_forms(Forms, Opts) end
   end.
 
--spec compile_forms([erl_parse:abstract_form()], options()) ->
-  atom() | undefined.
-compile_forms([], _) ->
-  undefined;
+-spec compile_forms(cerl:c_module(), options()) -> atom().
 compile_forms(Module, Opts) ->
   %% io:format("===== Module ====~n~s~n", [core_pp:format(Module)]),
   ok       = maybe_output_erl(Module, Opts),
@@ -315,27 +309,26 @@ add_core_code(BeamBinary, CoreModule) ->
   {ok, NewBeamBinary} = beam_lib:build_module(OldChunks ++ [CoreAbstractChunk]),
   NewBeamBinary.
 
--define(CERL_EVAL_MODULE, cerl_eval).
+-define(CERL_EVAL_MODULE, "cerl_eval").
 
--spec eval_expressions([erl_parse:abstract_expr()]) -> [any()].
+-spec eval_expressions([cerl:cerl()]) -> [any()].
 eval_expressions(Expressions) ->
-  %% io:format("==== EXPR ====~n~s~n", [ast_to_string(Expressions)]),
+  %% io:format("==== EXPR ====~n~s~n", [core_pp:format(EvalModule)]),
   CurrentNs     = clj_namespace:current(),
   CurrentNsSym  = clj_namespace:name(CurrentNs),
   CurrentNsAtom = binary_to_existing_atom(clj_core:str(CurrentNsSym), utf8),
   ReplacedExprs = [clj_module:replace_calls(Expr, CurrentNsAtom)
                    || Expr <- Expressions],
 
-  EvalModule = eval_module(ReplacedExprs),
-  %% io:format("========= Eval ========~n~s~n", [core_pp:format(EvalModule)]),
+  {ModuleName, EvalModule} = eval_module(ReplacedExprs),
   {ok, _, Binary} = compile:forms(EvalModule, [clint, from_core]),
-  code:load_binary(?CERL_EVAL_MODULE, "", Binary),
-  Value = cerl_eval:eval(),
-  code:purge(?CERL_EVAL_MODULE),
-  code:delete(?CERL_EVAL_MODULE),
+  code:load_binary(ModuleName, "", Binary),
+  Value = ModuleName:eval(),
+  code:purge(ModuleName),
+  code:delete(ModuleName),
   Value.
 
--spec eval_module([cerl:cerl()]) -> [any()].
+-spec eval_module([cerl:cerl()]) -> {module(), cerl:c_module()}.
 eval_module(Expressions) ->
   FunName = cerl:c_fname(eval, 0),
   FunBody = case Expressions of
@@ -345,16 +338,26 @@ eval_module(Expressions) ->
                end,
   Fun     = cerl:c_fun([], FunBody),
 
-  {InfoExports, InfoFuns} = clj_module:module_info_funs(?CERL_EVAL_MODULE),
+  ModuleName = eval_module_name(),
+  {InfoExports, InfoFuns} = clj_module:module_info_funs(ModuleName),
 
   Exports = [FunName | InfoExports],
-  Attrs   = [],
+  Attrs   = [{cerl:c_atom(clojure), cerl:abstract(true)}],
   Defs    = [{FunName, Fun} | InfoFuns],
-  Name    = cerl:c_atom(?CERL_EVAL_MODULE),
-  cerl:c_module(Name, Exports, Attrs, Defs).
+  Name    = cerl:c_atom(ModuleName),
+  {ModuleName, cerl:c_module(Name, Exports, Attrs, Defs)}.
 
--spec ast_to_string([erl_parse:abstract_form()]) -> string().
-ast_to_string(Forms) -> erl_prettypr:format(erl_syntax:form_list(Forms)).
+-spec eval_module_name() -> module().
+eval_module_name() ->
+  eval_module_name(0).
+
+-spec eval_module_name(non_neg_integer()) -> module().
+eval_module_name(N) ->
+  Name = list_to_atom(?CERL_EVAL_MODULE ++ integer_to_list(N)),
+  case code:is_loaded(Name) of
+    false -> Name;
+    _     -> eval_module_name(N + 1)
+  end.
 
 -spec when_verbose(options(), binary()) -> ok.
 when_verbose(#{verbose := true}, Message) ->
@@ -362,10 +365,10 @@ when_verbose(#{verbose := true}, Message) ->
 when_verbose(_, _) ->
   ok.
 
--spec maybe_output_erl([erl_parse:abstract_form()], options()) ->
+-spec maybe_output_erl(cerl:c_module(), options()) ->
   ok | {error, term()}.
-maybe_output_erl(Forms, #{output_erl := Filename}) ->
-  Source = ast_to_string(Forms),
+maybe_output_erl(Module, #{output_erl := Filename}) ->
+  Source = core_pp:format(Module),
   file:write_file(Filename, Source);
 maybe_output_erl(_, _) ->
   ok.
