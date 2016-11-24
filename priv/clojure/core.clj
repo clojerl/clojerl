d26fcf81220efd066ff3e25406aa96f4abb6855e
Juan Facorro <juan.facorro@gmail.com>
2016-11-24 18:20:06 +0100

    [#218] Uncomment map-indexed definition that uses letfn

--- priv/clojure/core.clj
+++ priv/clojure/core.clj
@@ -5949,18 +5949,17 @@
          ([result input]
           (rf result (f (vswap! i inc) input)))))))
   ([f coll]
-   (throw "unimplemented letfn*")
-   #_(letfn [(mapi [idx coll]
+   (letfn [(mapi [idx coll]
                  (lazy-seq
                    (when-let [s (seq coll)]
-                     (if (chunked-seq? s)
+                     #_(if (chunked-seq? s)
                        (let [c (chunk-first s)
                              size (int (count c))
                              b (chunk-buffer size)]
                          (dotimes [i size]
                            (chunk-append b (f (+ idx i) (.nth c i))))
-                         (chunk-cons (chunk b) (mapi (+ idx size) (chunk-rest s))))
-                       (cons (f idx (first s)) (mapi (inc idx) (rest s)))))))]
+                         (chunk-cons (chunk b) (mapi (+ idx size) (chunk-rest s)))))
+                     (cons (f idx (first s)) (mapi (inc idx) (rest s))))))]
      (mapi 0 coll))))
 
 (defn keep
