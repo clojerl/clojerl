32d3b035b6d85881c7229eeb71b19a9fad9adb68
Juan Facorro <juan.facorro@gmail.com>
2017-03-09 22:37:19 +0100

    [#111] Implement Stringable for Default type

--- src/lang/clojerl.Default.erl
+++ src/lang/clojerl.Default.erl
@@ -1,1 +1,16 @@
+%% Used to allow a 'default' symbol as a type in extend-type form as a
+%% catch all implementation
 -module('clojerl.Default').
+
+-include("clojerl.hrl").
+
+-behavior('clojerl.Stringable').
+
+-export([str/1]).
+
+%%------------------------------------------------------------------------------
+%% Protocols
+%%------------------------------------------------------------------------------
+
+str(_BitStr) ->
+  <<"#<default>">>.
