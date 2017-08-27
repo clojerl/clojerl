cfa483dddd27ebbcc3bca0e50444406bca4921cc
Juan Facorro <juan.facorro@klarna.com>
2017-08-27 12:34:21 +0200

    [#261] Use modified version of lists:seq for Range and handle step=0 correctly

--- src/erl/lang/collections/clojerl.Range.erl
+++ src/erl/lang/collections/clojerl.Range.erl
@@ -45,8 +45,9 @@
 -type type() :: #?TYPE{data :: {integer(), integer(), integer()}}.
 
 -spec ?CONSTRUCTOR(integer(), integer(), integer()) -> type().
-?CONSTRUCTOR(Start, End, Step) when Step >= 0, End =< Start;
-                                    Step < 0, Start =< End ->
+?CONSTRUCTOR(Start, End, Step) when Step > 0, End =< Start;
+                                    Step < 0, Start =< End;
+                                    Start == End ->
   [];
 ?CONSTRUCTOR(Start, End, Step) ->
   #?TYPE{data = {Start, End, Step}}.
@@ -66,10 +67,10 @@ empty(_) -> [].
 chunked_first(#?TYPE{name = ?M, data = {Start0, End0, Step0}}) ->
   End1    = Start0 + ?CHUNK_SIZE * Step0,
   End2    = case Step0 >= 0 of
-              true  -> lists:min([End0, End1]) - 1;
-              false -> lists:max([End0, End1]) + 1
+              true  -> lists:min([End0, End1]);
+              false -> lists:max([End0, End1])
             end,
-  Numbers = lists:seq(Start0, End2, Step0),
+  Numbers = to_list(Start0, End2, Step0),
   Tuple   = list_to_tuple(Numbers),
   'clojerl.TupleChunk':?CONSTRUCTOR(Tuple).
 
@@ -107,7 +108,7 @@ reduce(#?TYPE{name = ?M, data = {Start, End, Step}}, F, Init) ->
 
 do_reduce(_F, Acc, Start, End, Step) when
     Step >= 0, Start + Step > End;
-    Step < 1, Start + Step < End ->
+    Step < 0, Start + Step < End ->
   Acc;
 do_reduce(F, Acc, Start, End, Step) ->
   Val = clj_rt:apply(F, [Acc, Start]),
@@ -119,15 +120,15 @@ do_reduce(F, Acc, Start, End, Step) ->
 first(#?TYPE{name = ?M, data = {Start, _, _}}) -> Start.
 
 next(#?TYPE{name = ?M, data = {Start, End, Step}}) when
-    Step >= 0, Start + Step >= End;
-    Step < 1, Start + Step =< End ->
+    Step > 0, Start + Step >= End;
+    Step < 0, Start + Step =< End ->
   ?NIL;
 next(#?TYPE{name = ?M, data = {Start, End, Step}}) ->
   ?CONSTRUCTOR(Start + Step, End, Step).
 
 more(#?TYPE{name = ?M, data = {Start, End, Step}}) when
-    Step >= 0, Start + Step >= End;
-    Step < 1, Start + Step =< End ->
+    Step > 0, Start + Step >= End;
+    Step < 0, Start + Step =< End ->
   [];
 more(#?TYPE{name = ?M, data = {Start, End, Step}}) ->
   ?CONSTRUCTOR(Start + Step, End, Step).
@@ -136,10 +137,39 @@ more(#?TYPE{name = ?M, data = {Start, End, Step}}) ->
 
 seq(#?TYPE{name = ?M} = Seq) -> Seq.
 
-to_list(#?TYPE{name = ?M, data = {Start, End, Step}}) when Step >= 0 ->
-  lists:seq(Start, End - 1, Step);
-to_list(#?TYPE{name = ?M, data = {Start, End, Step}}) when Step < 0 ->
-  lists:seq(Start, End + 1, Step).
+to_list(#?TYPE{name = ?M, data = {Start, End, Step}}) ->
+  to_list(Start, End, Step).
 
 str(#?TYPE{name = ?M} = Range) ->
   clj_rt:print(Range).
+
+%%------------------------------------------------------------------------------
+%% Helper functions
+%%------------------------------------------------------------------------------
+
+-spec to_list(number(), number(), number()) -> [number()].
+to_list(Start, End, Step) when is_float(Start);
+                               is_float(End);
+                               is_float(Step) ->
+  N = clj_utils:ceil((End - Start) / Step),
+  to_list_loop(N, Start, Step, []);
+to_list(Start, End, Step) when Step >= 0 ->
+  lists:seq(Start, End - 1, Step);
+to_list(Start, End, Step) when Step < 0 ->
+  lists:seq(Start, End + 1, Step).
+
+%% We need to build the list from the Start to the End to avoid
+%% floating point rounding errors.
+-spec to_list_loop(integer(), number(), number(), [number()]) -> [number()].
+to_list_loop(N, X, Step, L) when N >= 4 ->
+  Y = X + Step,
+  Z = Y + Step,
+  W = Z + Step,
+  to_list_loop(N - 4, W + Step, Step, [W, Z, Y, X | L]);
+to_list_loop(N, X, Step, L) when N >= 2 ->
+  Y = X + Step,
+  to_list_loop(N - 2, Y + Step, Step, [Y, X | L]);
+to_list_loop(1, X, _, L) ->
+  lists:reverse([X | L]);
+to_list_loop(0, _, _, L) ->
+  lists:reverse(L).
