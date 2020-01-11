a2989e1c06e66c37402c5e1256645b1853fd60b1
Juan Facorro <juan.facorro@klarna.com>
2020-01-11 19:43:01 +0100

    [#681] Provide constructor for erlang.io.File type

--- src/erl/erlang/erlang.io.File.erl
+++ src/erl/erlang/erlang.io.File.erl
@@ -7,7 +7,11 @@
 -behavior('erlang.io.IWriter').
 -behavior('clojerl.IStringable').
 
--export([open/1, open/2, path/1]).
+-export([ ?CONSTRUCTOR/1
+        , open/1
+        , open/2
+        , path/1
+        ]).
 -export([make_temp/2]).
 
 -export([close/1]).
@@ -26,6 +30,10 @@
                  , pid   => pid() | file:fd()
                  , path  => path()
                  }.
+
+-spec ?CONSTRUCTOR(path()) -> type().
+?CONSTRUCTOR(Path) ->
+  open(Path).
 
 -spec open(path()) -> type().
 open(Path) when is_binary(Path) ->
