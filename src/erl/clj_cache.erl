7b501c468ecd7cca30a7ef1abbc3cbac6d372bc2
Juan Facorro <juan.facorro@klarna.com>
2020-08-18 00:24:16 +0200

    [#766] Add documentation to clj_cache

--- src/erl/clj_cache.erl
+++ src/erl/clj_cache.erl
@@ -1,3 +1,12 @@
+%% @doc Generic cache module
+%%
+%% Implements the `gen_server' behaviour and on initialization creates
+%% an in-memory ETS table where to keep the cached values.
+%%
+%% This cache is used for optimizing access to certain values. For
+%% example to avoid recalculating certain information regarding a
+%% module. Or to fetch the binary BEAM from memory instead of looking
+%% for it in a file's chunks.
 -module(clj_cache).
 
 -behavior(gen_server).
@@ -20,6 +29,7 @@
 %% Exported Functions
 %%------------------------------------------------------------------------------
 
+%% @doc Get the value for `Key' or `undefined' if not found.
 -spec get(any()) -> undefined | {ok, any()}.
 get(Key) ->
   case ets:lookup(?MODULE, Key) of
@@ -27,6 +37,7 @@ get(Key) ->
     [{Key, Value}] -> {ok, Value}
   end.
 
+%% @doc Add or update the `Value' for `Key'.
 -spec put(any(), any()) -> ok.
 put(Key, Value) ->
   gen_server:call(?MODULE, {put, Key, Value}).
@@ -35,25 +46,32 @@ put(Key, Value) ->
 %% gen_server callbacks
 %%------------------------------------------------------------------------------
 
+%% @private
 start_link() ->
   gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
+%% @private
 init([]) ->
   ets:new(?MODULE, [named_table, set, protected, {keypos, 1}]),
   {ok, undefined}.
 
+%% @private
 handle_call({put, Key, Value}, _From, State) ->
   true = ets:insert(?MODULE, {Key, Value}),
   {reply, ok, State}.
 
+%% @private
 handle_cast(_Msg, State) ->
   {noreply, State}.
 
+%% @private
 handle_info(_Msg, State) ->
   {noreply, State}.
 
+%% @private
 terminate(_Msg, State) ->
   {ok, State}.
 
+%% @private
 code_change(_Msg, _From, State) ->
   {ok, State}.
